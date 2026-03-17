ad0899513e22285b7e2884fa4981e493c3bf2a32
ivose <ivosek.faltus@gmail.com>
2026-03-17 21:08:44 +0100

    a

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const express = require("express")
-
+﻿const express = require("express")
+const path = require("path")
 
 const app = express()
-
 
 app.set("view engine","ejs")
 
 app.get('/',(req,res)=>{
-
-
-
-
-    res.render("purpose")
+    res.render("home")
 })
 
+app.get('/features',(req,res)=>{
+    res.render("features")
+})
 
+app.get('/why',(req,res)=>{
+    res.render("why")
+})
 
+app.get('/technologies',(req,res)=>{
+    res.render("technologies")
+})
+
+app.get('/getting-started',(req,res)=>{
+    res.render("getting-started")
+})
+
+app.get('/contact',(req,res)=>{
+    res.render("contact")
+})
+
+app.get('/poster',(req,res)=>{
+    res.sendFile(path.join(__dirname, "poster.pdf"))
+})
 
 app.listen(3000,()=>{
-
     console.log("App running on port 3000")
 })
